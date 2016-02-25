39f0dacca13235e0263355b94f5bad80ce7f9268
aminsa2000 <sabeti59@yahoo.com>
2016-02-25 12:35:44 +0330

    Create auto_leave.lua

---
+++ plugins/auto_leave.lua
@@ -0,0 +1,22 @@
+
+do
+-- Will leave the group if be added
+local function run(msg, matches)
+local bot_id = 177958856 -- your bot id
+   -- like local bot_id = 1234567
+    if matches[1] == 'left' and is_sudo(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+       send_large_msg("chat#id"..msg.to.id, 'دفه اخرت باشه بی اجازه منو ادد میکنی', ok_cb, false)
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^[!/](left)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
